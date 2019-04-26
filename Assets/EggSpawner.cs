c130ceeee8646a0765a20a69f1533c97779a2e94
a.kamaletdinov <a.kamaletdinov@corpwebgames.com>
2019-04-26 21:43:10 +0300

    Init

---
+++ Assets/EggSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EggSpawner : MonoBehaviour {
+    [SerializeField] private List<Transform> _eggSpawnPositions;
+    [SerializeField] private GameObject Egg;
+    private float _currentCoolDownTime;
+    private float _timer;
+
+    private void SpawnRandomEgg() {
+        var num = Random.Range(0, 3);
+        switch (num) {
+            case 0:
+                Instantiate(Egg, _eggSpawnPositions[0].position, Quaternion.identity);
+                break;
+            case 1:
+                Instantiate(Egg, _eggSpawnPositions[1].position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(Egg, _eggSpawnPositions[2].position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(Egg, _eggSpawnPositions[3].position, Quaternion.identity);
+                break;
+        }
+    }
+
+    private void Update() {
+        _timer += Time.deltaTime;
+        
+        if (_currentCoolDownTime == 0)
+            _currentCoolDownTime = Random.Range(0.7f, 2f);
+
+        if (_timer > _currentCoolDownTime) {
+            SpawnRandomEgg();
+            _timer = 0;
+        }
+            
+    }
+}
---
+++ Assets/bascketControls.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bascketControls : MonoBehaviour {
+    
+    [SerializeField] private Transform basket;
+
+    [SerializeField] private Transform _leftUp;
+    [SerializeField] private Transform _rightUp;
+    [SerializeField] private Transform _leftDown;
+    [SerializeField] private Transform _rightDown;
+
+    private bool _isUp;
+    private bool _isLeft;
+
+    private void Start() {
+        basket.position = _leftUp.position;
+    }
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            _isUp = true;
+        }  
+        
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            _isUp = false;
+        } 
+        
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            _isLeft = true;
+        } 
+        
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            _isLeft = false;
+        }
+
+        if (_isUp && _isLeft)
+            basket.position = _leftUp.position;
+        
+        if (_isUp && !_isLeft)
+            basket.position = _rightUp.position;
+        
+        if (!_isUp && _isLeft)
+            basket.position = _leftDown.position;
+        
+        if (!_isUp && !_isLeft)
+            basket.position = _rightDown.position;
+        
+        
+
+
+
+    } 
+}
